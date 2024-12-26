2bf60585b5dbe1de1288bf4ae131d91f0db2f196
Edrick Sinsuan <evcsinsuan@gmail.com>
2024-12-25 22:25:48 -0500

    clk: qcom: gcc/mmcc-msm8998: Follow downstream's has_sibling configs
    In k4.4, branch clocks are configured to set their parent's rate
    through the has_sibling config, if the branch clock has no sibling
    that's only when the branch clock should set the parent's clock rate.
    
    Now, in k4.14, Sony's port didn't seem to have followed downstream
    in some areas such as the camera clocks. As a result, this has caused
    4k recording to not function correctly as the clock sources get
    conflicting rates from branch clocks that were not supposed to
    propagate their rate to their parent.
    
    To fix the 4k recording issue, as well as possibly other issues
    caused by this diff, follow downstream's configuration of
    CLK_SET_RATE_PARENT.
    
    Signed-off-by: Edrick Sinsuan <evcsinsuan@gmail.com>

--- drivers/clk/qcom/gcc-msm8998.c
+++ drivers/clk/qcom/gcc-msm8998.c
@@ -1990,7 +1990,6 @@ static struct clk_branch gcc_cfg_noc_usb3_axi_clk = {
 				"usb30_master_clk_src"
 			},
 			.num_parents = 1,
-			//.flags = CLK_SET_RATE_PARENT, //HANDOFF?
 			.ops = &clk_branch2_ops,
 		},
 	},
@@ -2213,7 +2212,6 @@ static struct clk_branch gcc_pcie_phy_aux_clk = {
 				"pcie_aux_clk_src"
 			},
 			.num_parents = 1,
-			.flags = CLK_SET_RATE_PARENT,
 			.ops = &clk_branch2_ops,
 		},
 	},
